5f63b50ba4a9a848f06b0cf3ede39dd3abe1f44e
vedant234 <74408696+vedant234@users.noreply.github.com>
2021-04-14 16:40:50 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# newton-cradle-t
+Blueprint for sling and pendulum, add objects by creating variables, calling constructor and display methods
---
+++ sketch.js
@@ -0,0 +1,59 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Mouse = Matter.Mouse;
+const Constraint = Matter.Constraint;
+
+const MouseConstraint = Matter.MouseConstraint;
+
+var world, engine, canvas;
+var pendulum1, pendulum2, pendulum3, pendulum4, pendulum5;
+var sling1, sling2, sling3, sling4, sling5;
+var mConstraint;
+
+function setup() {
+  canvas = createCanvas(windowWidth / 2, windowHeight / 1.5);
+  engine = Engine.create();
+  world = engine.world;
+
+  let canvasmouse = Mouse.create(canvas.elt);
+  canvasmouse.pixelRatio = pixelDensity();
+  let options = {
+    mouse: canvasmouse
+  };
+  mConstraint = MouseConstraint.create(engine, options);
+  World.add(world, mConstraint);
+
+  pendulum1 = new Pendulum(340, 450, "green");
+  pendulum2 = new Pendulum(400, 450, "yellow");
+  pendulum3 = new Pendulum(460,450,"red");
+  pendulum4 = new Pendulum(520,450,"blue");
+ 
+
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, {  x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, {  x: 460,y:200});
+  sling4  = new Sling(pendulum4.body, {  x: 520,y:200});
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+
+  pendulum1.display();
+  pendulum2.display();
+  pendulum3.display();
+  pendulum4.display();
+  
+  
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+}
+
+
